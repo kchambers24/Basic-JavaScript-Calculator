9d1f142cc0267ffedc3effeb96a186334b7dae24
Kevin Chambers <kbchambers2@gmail.com>
2016-07-27 09:01:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+var title = "Adding Is Fun - JS Calculate!"
+alert("Title: " + title);
 
+//var a = document.getElementById(“input1”).value;
+//var b = document.getElementById(“input2”).value;{
+
+//var answer = (a + b);
+ //var sum = answer
+ //button is an object
+//var bSolution = document.querySelector(“button”);
+ //bSolution.addEventListener(“click”, function(event){
+
+
+ //var firstNumber = document.getElementById("input1").value;
+ //var secondNumber = document.getElementById("input1").value;
+
+
+
+ var firstNumber = document.getElementById("input1").valueAsNumber;
+ var secondNumber = document.getElementById("input2").valueAsNumber;
+
+ var buttonElement = document.querySelector(“button”);
+ buttonElement.addEventListener("click", function(event){
+   var result = firstNumber + secondNumber;
+   console.log(result);
+ }
+
+
+ //var sumNumber = (firstNumber + secondNumber)
+
+
+//var calculate = document.querySelector("input");
